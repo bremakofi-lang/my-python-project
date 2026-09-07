56f527d8c7cea603b110347a357f2e93c12c3be4
bremakofi-lang <bremakofi@gmail.com>
2026-09-07 16:20:26 +0300

    Add files via upload

---
+++ simple_bill_calculator.py
@@ -0,0 +1,8 @@
+# my python project
+price = float(input('How much is for this book?'))
+quantity = int(input('How many books do you want?'))
+total = price * quantity
+print(f'Price of the book is: {price}')
+print(f'Quantity of the book is: {quantity}')
+print(f'Total price is: {total}')
+                     
